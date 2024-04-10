67e174c5be6c55c3e0331f7075ac96cf749868a4
cakekangaroo <129749114+cakekangaroo@users.noreply.github.com>
2024-04-10 11:34:12 +0000

    bls init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ZippyChain/zippybls
+
+go 1.19
---
+++ obj/bls_c384_256.d
@@ -0,0 +1,52 @@
+obj/bls_c384_256.o: src/bls_c384_256.cpp src/bls_c_impl.hpp \
+ include/bls/bls.h \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/bn.h \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/curve_type.h \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/impl/bn_c_impl.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/bls12_381.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/bn.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/fp_tower.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/fp.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/hash.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/inttype.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/stream.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/exception.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/op.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/gmp_util.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/randgen.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/random_generator.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/array.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/util.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/bit_operation.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/operator.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/conversion.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/itoa.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/ec.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/lagrange.hpp \
+ /home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/ecparam.hpp
+src/bls_c_impl.hpp:
+include/bls/bls.h:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/bn.h:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/curve_type.h:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/impl/bn_c_impl.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/bls12_381.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/bn.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/fp_tower.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/fp.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/hash.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/inttype.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/stream.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/exception.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/op.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/gmp_util.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/randgen.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/random_generator.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/array.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/util.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/bit_operation.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/operator.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/conversion.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/cybozu/itoa.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/ec.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/lagrange.hpp:
+/home/zippy001/go/src/github.com/harmony-one/bls/../mcl/include/mcl/ecparam.hpp:
